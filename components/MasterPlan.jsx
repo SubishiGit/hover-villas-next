2d08bad3b55a71f051b8a4ee6fc843c0b517e064
Rishik Thimmaiahgari <rishikthimmaiahgari@Rishiks-MacBook-Pro-2.local>
2025-09-25 11:47:47 +0530

    feat: Add glowing text effect to tooltip status

--- components/MasterPlan.jsx
+++ components/MasterPlan.jsx
@@ -161,8 +161,9 @@ export default function MasterPlan({ mapData, sheetRows = [] }) {
             alt="Master Plan Base Layer"
             style={{ 
               position: 'absolute',
-              top: 0,
-              left: 0,
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
               width: '100%',
               height: '100%',
               objectFit: 'contain',
